267bcc78eda9007a7c0453ea69adb9128ce03d9d
Daniel Ferreira <d2fgithub@gmail.com>
2024-07-19 15:40:39 -0300

    Adição de 2 funções.
    Adição de funções para verificar Palíndromo e máximo e mínimo dentro de um Array.

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+  let strModified = str.toLowerCase().replace(/ /g, "");
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str.toLowerCase()[i].replace(/ /g, "");
+  }
+  if (result === strModified) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+  let numberMax = 0;
+  let numberMin = arr[0];
+  
+  for (let i = 0; i < arr.length; i++) {
+    let position = arr[i];
+    if (position > numberMax) {
+      numberMax = position;
+    } else if (position < numberMin) {
+      numberMin = position;
+    }
+  }
+  let result = [`${numberMin}`, `${numberMax}`]
+  return result
 }
